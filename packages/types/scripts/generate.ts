592d4975bb8408adf88342d589c703d96f07b31f
Bryan Chen <xlchen1291@gmail.com>
2019-11-29 14:05:03 +1300

    update code generating script

--- packages/types/scripts/generate.ts
+++ packages/types/scripts/generate.ts
@@ -1,7 +1,16 @@
 import { generateInterfaceRegistry } from '@polkadot/types/scripts/generateTypes/interfaceRegistry';
 import { generateTsDef } from '@polkadot/types/scripts/generateTypes/tsDef';
+import * as defaultDefinations from '@polkadot/types/interfaces/definitions';
 
-import * as definations from '../src/interfaces/definitions';
+import * as ormlDefinations from '../src/interfaces/definitions';
 
-generateTsDef(definations, 'packages/types/src/interfaces');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { runtime, ...substrateDefinations } = defaultDefinations;
+
+const definations = {
+  '@polkadot/types/interfaces': substrateDefinations,
+  '@orml/types/interfaces': ormlDefinations
+};
+
+generateTsDef(definations, 'packages/types/src/interfaces', '@orml/types/interfaces');
 generateInterfaceRegistry(definations, 'packages/types/src/interfaceRegistry.ts');
--- packages/types/src/interfaces/runtime/definitions.ts
+++ packages/types/src/interfaces/runtime/definitions.ts
@@ -1,5 +1,8 @@
+import definitions from '@polkadot/types/interfaces/runtime/definitions';
+
 export default {
   types: {
+    ...definitions.types,
     OracleValue: 'FixedU128'
   }
 };
