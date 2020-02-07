776f709f5945b1a6055a7f61d81bf759b717013f
Shaopeng Wang <spxwang@gmail.com>
2020-02-07 14:23:07 +1300

    Type and linting workarounds.

--- packages/jsonrpc/src/oracle.ts
+++ packages/jsonrpc/src/oracle.ts
@@ -1,13 +1,15 @@
-import { RpcMethodOpt } from '@polkadot/jsonrpc/types';
 import createMethod from '@polkadot/jsonrpc/create/method';
 import createParam from '@polkadot/jsonrpc/create/param';
 
 const section = 'oracle';
 
-const getValue: RpcMethodOpt = {
+// FIXME: once polkadotjs fixes the types inconsistency.
+// 1. change `getValue` to a concrete type.
+// 2. `createParam('key', 'OracleKey')`.
+const getValue: any = {
   description: 'Retrieves the oracle value for a given key.',
   params: [
-    createParam('key', 'OracleKey')
+    createParam('key', 'OracleKey' as 'u8')
   ],
   type: 'Option<TimestampedValue>'
 };
