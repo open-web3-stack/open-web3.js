0c6f94687124531b3523c4d76ed9110664690284
yznt <nsso@foxmail.com>
2020-02-17 17:23:21 +1300

    add api derive

---
+++ packages/api-derive/src/currencies/balance.ts
@@ -0,0 +1,23 @@
+import { ApiInterfaceRx } from '@polkadot/api/types';
+
+import { AccountId, AccountIndex, Address, Balance } from '@orml/types/interfaces';
+import { Observable } from 'rxjs';
+import { memo } from '../util';
+
+export function balance(
+  api: ApiInterfaceRx
+): (address: AccountIndex | AccountId | Address | string, token: any) => Observable<Balance> {
+  return memo(
+    (address: AccountIndex | AccountId | Address | string, token: any): Observable<Balance> => {
+      const CurrencyId = api.registry.get('CurrencyId');
+      const currencyId = new (CurrencyId as any)(api.registry, token);
+      const nativeCurrencyId = api.consts.currencies.nativeCurrencyId;
+
+      if (currencyId.toHex() === nativeCurrencyId.toHex()) {
+        return api.query.balances.freeBalance<Balance>(address as any);
+      } else {
+        return api.query.tokens.balance<Balance>(token, address as any);
+      }
+    }
+  );
+}
