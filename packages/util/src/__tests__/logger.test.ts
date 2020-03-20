74beed48538a7aa57984f98a51af392fa9d62778
Bryan Chen <xlchen1291@gmail.com>
2020-03-20 14:06:35 +1300

    add logger

---
+++ packages/util/src/__tests__/logger.test.ts
@@ -0,0 +1,198 @@
+import { Logger, LoggerPayload, createFilterOutput, LoggerLevel, createBufferedOutput } from '../logger';
+
+describe('Logger', () => {
+  let logger: Logger;
+  let logs: LoggerPayload[];
+
+  beforeEach(() => {
+    logs = []
+    logger = new Logger();
+    logger.addMiddleware(payload => logs.push(payload));
+  })
+
+  it('logs with different levels', () => {
+    logger.debug('debug');
+    logger.log('log');
+    logger.warn('warn');
+    logger.error('error');
+
+    expect(logs).toEqual([{
+      level: 'debug',
+      args: ['debug'],
+      timestamp: expect.any(Date),
+      namespaces: [],
+    }, {
+      level: 'log',
+      args: ['log'],
+      timestamp: expect.any(Date),
+      namespaces: [],
+    }, {
+      level: 'warn',
+      args: ['warn'],
+      timestamp: expect.any(Date),
+      namespaces: [],
+    }, {
+      level: 'error',
+      args: ['error'],
+      timestamp: expect.any(Date),
+      namespaces: [],
+    }])
+  })
+
+  describe('filterOutput', () => {
+    beforeEach(() => {
+      logger.addMiddleware(createFilterOutput(LoggerLevel.Warn));
+    })
+
+    it('filter logs', () => {
+      logger.debug('debug');
+      logger.log('log');
+      logger.warn('warn');
+      logger.error('error');
+
+      expect(logs).toEqual([{
+        level: 'warn',
+        args: ['warn'],
+        timestamp: expect.any(Date),
+        namespaces: [],
+      }, {
+        level: 'error',
+        args: ['error'],
+        timestamp: expect.any(Date),
+        namespaces: [],
+      }])
+    })
+  })
+
+  describe('bufferedOutput', () => {
+    beforeEach(() => {
+      logger.addMiddleware(createBufferedOutput(LoggerLevel.Warn, 3))
+    })
+
+    it('buffer logs', () => {
+      logger.debug(1)
+      logger.debug(2)
+      logger.debug(3)
+      logger.debug(4)
+
+      expect(logs).toEqual([])
+
+      logger.warn(5)
+
+      expect(logs).toEqual([{
+        level: 'debug',
+        args: [2],
+        timestamp: expect.any(Date),
+        namespaces: [],
+      }, {
+        level: 'debug',
+        args: [3],
+        timestamp: expect.any(Date),
+        namespaces: [],
+      }, {
+        level: 'debug',
+        args: [4],
+        timestamp: expect.any(Date),
+        namespaces: [],
+      }, {
+        level: 'warn',
+        args: [5],
+        timestamp: expect.any(Date),
+        namespaces: [],
+      }])
+    })
+  })
+
+  describe('with child logger', () => {
+    let childLogger: Logger;
+    let childLogger2: Logger;
+    let nestedChildLogger: Logger;
+
+    beforeEach(() => {
+      childLogger = logger.createLogger('child');
+      childLogger2 = logger.createLogger('child2');
+      nestedChildLogger = childLogger.createLogger('nested');
+    })
+
+    it('logs with namespaces', () => {
+      childLogger.info(1, 2)
+      childLogger2.info([3, 4])
+      nestedChildLogger.log({ '5': 6 })
+
+      expect(logs).toEqual([{
+        level: 'info',
+        args: [1, 2],
+        timestamp: expect.any(Date),
+        namespaces: ['child'],
+      }, {
+        level: 'info',
+        args: [[3, 4]],
+        timestamp: expect.any(Date),
+        namespaces: ['child2'],
+      }, {
+        level: 'log',
+        args: [{ '5': 6 }],
+        timestamp: expect.any(Date),
+        namespaces: ['child', 'nested'],
+      },])
+    })
+
+    it('can apply middleware on child logger', () => {
+      childLogger.addMiddleware((payload, next) => next({ ...payload, args: payload.args.concat('child') }));
+      childLogger2.addMiddleware((payload, next) => next({ ...payload, args: payload.args.concat('child2') }));
+      nestedChildLogger.addMiddleware((payload, next) => next({ ...payload, args: payload.args.concat('nested') }));
+
+      childLogger.info(1, 2)
+      childLogger2.info([3, 4])
+      nestedChildLogger.log({ '5': 6 })
+
+      expect(logs).toEqual([{
+        level: 'info',
+        args: [1, 2, 'child'],
+        timestamp: expect.any(Date),
+        namespaces: ['child'],
+      }, {
+        level: 'info',
+        args: [[3, 4], 'child2'],
+        timestamp: expect.any(Date),
+        namespaces: ['child2'],
+      }, {
+        level: 'log',
+        args: [{ '5': 6 }, 'nested', 'child'],
+        timestamp: expect.any(Date),
+        namespaces: ['child', 'nested'],
+      },])
+    })
+
+    it('modify parent middleware are applied on children', () => {
+      logger.addMiddleware((payload, next) => next({ ...payload, args: payload.args.concat('logger') }));
+
+      logger.debug('0')
+      childLogger.info(1, 2)
+      childLogger2.info([3, 4])
+      nestedChildLogger.log({ '5': 6 })
+
+      expect(logs).toEqual([{
+        level: 'debug',
+        args: ['0', 'logger'],
+        timestamp: expect.any(Date),
+        namespaces: [],
+      }, {
+        level: 'info',
+        args: [1, 2, 'logger'],
+        timestamp: expect.any(Date),
+        namespaces: ['child'],
+      }, {
+        level: 'info',
+        args: [[3, 4], 'logger'],
+        timestamp: expect.any(Date),
+        namespaces: ['child2'],
+      }, {
+        level: 'log',
+        args: [{ '5': 6 }, 'logger'],
+        timestamp: expect.any(Date),
+        namespaces: ['child', 'nested'],
+      },])
+    })
+  })
+})
---
+++ packages/util/src/logger.ts
@@ -0,0 +1,139 @@
+export enum LoggerLevel {
+  Debug = 'debug',
+  Log = 'log',
+  Info = 'info',
+  Warn = 'warn',
+  Error = 'error',
+}
+
+export const levelToNumber = (level: LoggerLevel) => {
+  switch (level) {
+    case LoggerLevel.Debug:
+      return 0;
+    case LoggerLevel.Log:
+      return 1;
+    case LoggerLevel.Info:
+      return 2;
+    case LoggerLevel.Warn:
+      return 3;
+    case LoggerLevel.Error:
+      return 4;
+  }
+};
+
+export interface LoggerPayload {
+  level: LoggerLevel;
+  args: any[];
+  timestamp: Date;
+  namespaces: string[];
+}
+
+export type LoggerMiddleware = (payload: LoggerPayload, next: LoggerOutput) => void;
+export type LoggerOutput = (payload: LoggerPayload) => void;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const noop = () => {};
+
+export class Logger {
+  private output: LoggerOutput;
+  private readonly namespaces: string[] = [];
+
+  constructor(namespace?: string, parent?: Logger) {
+    const parentNamespace = parent?.namespaces || [];
+    if (namespace) {
+      this.namespaces = parentNamespace.concat(namespace);
+    } else {
+      this.namespaces = parentNamespace;
+    }
+
+    if (parent) {
+      this.output = payload => parent.output(payload);
+    } else {
+      this.output = noop;
+    }
+  }
+
+  public debug(...args: any[]) {
+    this.logRaw(LoggerLevel.Debug, args);
+  }
+
+  public log(...args: any[]) {
+    this.logRaw(LoggerLevel.Log, args);
+  }
+
+  public info(...args: any[]) {
+    this.logRaw(LoggerLevel.Info, args);
+  }
+
+  public warn(...args: any[]) {
+    this.logRaw(LoggerLevel.Warn, args);
+  }
+
+  public error(...args: any[]) {
+    this.logRaw(LoggerLevel.Error, args);
+  }
+
+  public logRaw(level: LoggerLevel, args: any[]) {
+    this.output({
+      level, args, timestamp: new Date(), namespaces: this.namespaces
+    });
+  }
+
+  public addMiddleware(...middlewares: LoggerMiddleware[]) {
+    const output = middlewares.reduceRight((output: LoggerOutput, middleware) => payload => middleware(payload, output), this.output);
+    this.output = output;
+  }
+
+  public outOutput(output: LoggerOutput) {
+    this.output = output;
+  }
+
+  public createLogger(namespace?: string) {
+    return new Logger(namespace || Math.random().toString(36).substr(2, 5), this);
+  }
+
+  public static defaultInstance = new Logger('')
+}
+
+export const consoleOutput = (payload: LoggerPayload, next: LoggerOutput) => {
+  const labels: any[] = [payload.timestamp, payload.level.toUpperCase().padStart(5)];
+  if (payload.namespaces.length) {
+    labels.push('[' + payload.namespaces.join(':') + ']:');
+  } else {
+    labels[1] += ':';
+  }
+  console[payload.level](...labels, ...payload.args);
+  next(payload);
+};
+
+export const createFilterOutput = (level: LoggerLevel) => {
+  const num = levelToNumber(level);
+  return (payload: LoggerPayload, next: LoggerOutput) => {
+    if (levelToNumber(payload.level) >= num) {
+      next(payload);
+    }
+  };
+};
+
+export const createBufferedOutput = (level = LoggerLevel.Warn, size = 50) => {
+  const buffer: LoggerPayload[] = [];
+  const num = levelToNumber(level);
+  return (payload: LoggerPayload, next: LoggerOutput) => {
+    if (levelToNumber(payload.level) >= num) {
+      for (const log of buffer) {
+        next(log);
+      }
+      next(payload);
+      buffer.length = 0;
+    } else {
+      buffer.push(payload);
+      if (buffer.length > size) {
+        buffer.shift();
+      }
+    }
+  };
+};
+
+Logger.defaultInstance.addMiddleware(consoleOutput);
+
+export default Logger.defaultInstance;
