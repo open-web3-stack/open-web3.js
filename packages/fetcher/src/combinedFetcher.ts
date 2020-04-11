57882b3d5da000afb068dfb9d71667ce6be6d766
Ermal Kaleci <ermalkaleci@gmail.com>
2020-04-11 12:41:15 +0200

    add logger

--- packages/fetcher/src/combinedFetcher.ts
+++ packages/fetcher/src/combinedFetcher.ts
@@ -1,5 +1,8 @@
 import bn from 'big.js';
 import { FetcherInterface, Pair } from '../interfaces';
+import moduleLogger from './logger';
+
+const logger = moduleLogger.createLogger('CombinedFetcher');
 
 const median = (pricesUnsorted: string[]): string => {
   const prices = pricesUnsorted.sort();
@@ -31,12 +34,26 @@ export default class CombinedFetcher implements FetcherInterface {
   }
 
   async getPrice(pair: Pair): Promise<string> {
-    const results = await Promise.all(this.fetchers.map((fetcher) => fetcher.getPrice(pair).catch((e) => e)));
+    // fetch from all sources
+    const results = await Promise.all(
+      this.fetchers.map((fetcher) =>
+        fetcher.getPrice(pair).catch((error) => {
+          logger.warn('getPrice', { pair, error });
+          return error;
+        })
+      )
+    );
+
+    // get prices
     const prices = results.filter((i) => typeof i === 'string') as string[];
+
+    // ensure enough prices
     if (prices.length < this.minCount) {
       const errors = results.filter((i) => i instanceof Error);
       throw new CombinedFetcherError('not enough prices', errors);
     }
+
+    // return median
     return median(prices);
   }
 }
