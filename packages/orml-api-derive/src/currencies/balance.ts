d48e4550fa2825ce1bc7eb6597c8f60003a5ff1c
Bryan Chen <xlchen1291@gmail.com>
2020-06-03 15:25:38 +1200

    update parameter order

--- packages/orml-api-derive/src/currencies/balance.ts
+++ packages/orml-api-derive/src/currencies/balance.ts
@@ -11,8 +11,7 @@ export function balance(
 ): (address: AccountId | string | Uint8Array, token: any) => Observable<Balance> {
   return memo(
     (address: AccountId | string | Uint8Array, token: any): Observable<Balance> => {
-      const CurrencyId = api.registry.get('CurrencyId');
-      const currencyId = new (CurrencyId as any)(api.registry, token);
+      const currencyId = api.registry.createType('CurrencyId' as any, token);
       const nativeCurrencyId = api.consts.currencies.nativeCurrencyId;
 
       if (currencyId.eq(nativeCurrencyId)) {
@@ -22,7 +21,7 @@ export function balance(
           })
         );
       } else {
-        return api.query.tokens.accounts<OrmlAccountData>(token, address).pipe(
+        return api.query.tokens.accounts<OrmlAccountData>(address, token).pipe(
           map((result) => {
             return result.free;
           })
