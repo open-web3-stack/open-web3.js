0233572f852f8f0841dc838969cbaf3f6bbb2775
qiuhao <qiuhao951@gmail.com>
2020-09-07 18:20:54 +0800

    update-oracle-rpc

--- packages/orml-types/src/interfaces/oracle/types.ts
+++ packages/orml-types/src/interfaces/oracle/types.ts
@@ -2,7 +2,11 @@
 /* eslint-disable */
 
 import { Struct, Vec } from '@polkadot/types/codec';
+import { u8 } from '@polkadot/types/primitive';
 import { AccountId, Moment, OracleValue } from '@open-web3/orml-types/interfaces/runtime';
+
+/** @name DataProviderId */
+export interface DataProviderId extends u8 {}
 
 /** @name OrderedSet */
 export interface OrderedSet extends Vec<AccountId> {}
