9db6ebf61d8e651e925b0f5c774a33c5e245a506
qiuhao <qiuhao951@gmail.com>
2020-09-10 15:03:47 +0800

    update polkadot.js version & add instanceId (#127)
    update polkadot.js version & update api-derive

--- packages/orml-api-derive/src/index.ts
+++ packages/orml-api-derive/src/index.ts
@@ -1,5 +1,7 @@
+import { DeriveCustom } from '@polkadot/api-derive';
+
 import * as currencies from './currencies';
 
-export const derive = {
+export const derive: DeriveCustom = {
   currencies
 };
