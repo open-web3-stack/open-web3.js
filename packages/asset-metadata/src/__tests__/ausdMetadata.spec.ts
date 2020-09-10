cfc7fcdc35cddbc96f33af9ba25116dfb2706f98
aniiantt <nsso@foxmail.com>
2020-09-10 17:42:21 +1200

    update metadata

--- packages/asset-metadata/src/__tests__/ausdMetadata.spec.ts
+++ packages/asset-metadata/src/__tests__/ausdMetadata.spec.ts
@@ -1,13 +1,13 @@
 import { ApiRx, WsProvider } from '@polkadot/api';
 import AssetMetadata from '../AssetMetadata';
-import metadata from './acalaAssetMetadata';
+import ausdDef from './ausdMetadata.spec';
 
 describe('AssetMetadata', (): void => {
   it('new', () => {
     const provider = new WsProvider('ws://127.0.0.1:9944');
 
-    const assets = new AssetMetadata(new ApiRx({ provider }), metadata.assets[0]);
+    const ausd = new AssetMetadata(new ApiRx({ provider }), ausdDef);
 
-    expect(assets).toBeDefined();
+    expect(ausd).toBeDefined();
   });
 });
