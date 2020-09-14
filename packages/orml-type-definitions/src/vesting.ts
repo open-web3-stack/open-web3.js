38ff31ffdfe8a1c96ddaa7b5d918d93225383ec5
aniiantt <nxiiun@gmail.com>
2020-09-15 10:43:41 +1200

    resolve name

--- packages/orml-type-definitions/src/vesting.ts
+++ packages/orml-type-definitions/src/vesting.ts
@@ -1,12 +1,12 @@
 export default {
   rpc: {},
   types: {
-    VestingSchedule: {
+    OrmlVestingSchedule: {
       start: 'BlockNumber',
       period: 'BlockNumber',
       periodCount: 'u32',
       perPeriod: 'Compact<Balance>'
     },
-    VestingScheduleOf: 'VestingSchedule'
+    VestingScheduleOf: 'OrmlVestingSchedule'
   }
 };
