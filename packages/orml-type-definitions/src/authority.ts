9da5d29ddbe1e30853c3fc2892cadc36779d7989
aniiantt <nxiiun@gmail.com>
2020-10-08 13:52:06 +1300

    add types def

--- packages/orml-type-definitions/src/authority.ts
+++ packages/orml-type-definitions/src/authority.ts
@@ -1,6 +1,16 @@
 export default {
   rpc: {},
   types: {
-    CallOf: 'Call'
+    CallOf: 'Call',
+    DispatchTime: {
+      _enum: {
+        At: 'BlockNumber',
+        After: 'BlockNumber'
+      }
+    },
+    AsOriginId: {
+      _enum: ['Root', 'AcalaTreasury', 'HonzonTreasury', 'HomaTreasury', 'DSWF']
+    },
+    ScheduleTaskIndex: 'u32'
   }
 };
