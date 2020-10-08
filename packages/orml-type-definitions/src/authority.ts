61a7d9c16505f28e49533d6d5c1aa3e38c77150c
aniiantt <nxiiun@gmail.com>
2020-10-08 13:56:11 +1300

    move asoriginid

--- packages/orml-type-definitions/src/authority.ts
+++ packages/orml-type-definitions/src/authority.ts
@@ -8,9 +8,6 @@ export default {
         After: 'BlockNumber'
       }
     },
-    AsOriginId: {
-      _enum: ['Root', 'AcalaTreasury', 'HonzonTreasury', 'HomaTreasury', 'DSWF']
-    },
     ScheduleTaskIndex: 'u32'
   }
 };
