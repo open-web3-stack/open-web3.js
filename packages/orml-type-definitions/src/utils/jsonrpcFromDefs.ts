d6e0ce999e567f646da3df82fda6b9642122f348
aniiantt <nxiiun@gmail.com>
2020-10-13 20:50:50 +1300

    All types combine into one (#142)
    * all type in one
    
    * fix lint

---
+++ packages/orml-type-definitions/src/utils/jsonrpcFromDefs.ts
@@ -0,0 +1,24 @@
+export default function jsonrpcFromDefs(
+  definitions: Record<string, { rpc?: Record<string, any> }>
+): Record<string, Record<string, any>> {
+  const jsonrpc: Record<string, Record<string, any>> = {};
+
+  Object.keys(definitions)
+    .filter((key) => Object.keys(definitions[key]?.rpc || {}).length !== 0)
+    .forEach((section): void => {
+      jsonrpc[section] = {};
+      Object.entries(definitions[section].rpc as Record<string, any>).forEach(([method, def]): void => {
+        const isSubscription = !!def.pubsub;
+
+        jsonrpc[section][method] = {
+          ...def,
+          isSubscription,
+          jsonrpc: `${section}_${method}`,
+          method,
+          section
+        };
+      });
+    });
+
+  return jsonrpc;
+}
---
+++ packages/orml-type-definitions/src/utils/typesFromDefs.ts
@@ -0,0 +1,11 @@
+export default function typesFromDefs(
+  definitions: Record<string, { types: Record<string, any> }>
+): Record<string, any> {
+  return Object.values(definitions).reduce(
+    (res: Record<string, any>, { types }): Record<string, any> => ({
+      ...res,
+      ...types
+    }),
+    {}
+  );
+}
