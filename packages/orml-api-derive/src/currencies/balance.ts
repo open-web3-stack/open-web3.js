f2108ef8428db2ef60bc20f76ad7a98aef90aaab
qwer951123 <qiuhao951@gmail.com>
2021-01-04 00:43:00 +0800

    update orml-api-derive

--- packages/orml-api-derive/src/currencies/balance.ts
+++ packages/orml-api-derive/src/currencies/balance.ts
@@ -3,7 +3,7 @@ import { memo } from '@polkadot/api-derive/util';
 import { AccountInfo } from '@polkadot/types/interfaces';
 import { AccountId, Balance, OrmlAccountData } from '@open-web3/orml-types/interfaces';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, mergeMap } from 'rxjs/operators';
 
 export function balance(
   instanceId: string,
@@ -12,24 +12,34 @@ export function balance(
   return memo(
     instanceId,
     (address: AccountId | string | Uint8Array, token: any): Observable<Balance> => {
-      const currencyId = api.registry.createType('CurrencyId' as any, token);
-      const nativeCurrencyId = api.consts.currencies.nativeCurrencyId;
+      return api.rpc.system.properties().pipe(
+        mergeMap((properties) => {
+          const currencyId = api.registry.createType('CurrencyId' as any, token);
+          const nativeTokenSymbol = properties.tokenSymbol.unwrapOrDefault().toString();
+          const nativeCurrencyId = api.registry.createType(
+            'CurrencyId' as any,
+            (api.registry.getDefinition('CurrencyId') || '').includes('"Token":"TokenSymbol"')
+              ? { Token: nativeTokenSymbol }
+              : nativeTokenSymbol
+          );
 
-      if (currencyId.eq(nativeCurrencyId)) {
-        return api.query.system.account<AccountInfo>(address).pipe(
-          map((result) => {
-            return result.data.free;
-          })
-        );
-      } else {
-        const key1 = api.query.tokens.accounts.creator.meta.type.asDoubleMap.key1.toString();
-        const arg = key1 === 'CurrencyId' ? [token, address] : [address, token];
-        return api.query.tokens.accounts<OrmlAccountData>(...arg).pipe(
-          map((result) => {
-            return result.free;
-          })
-        );
-      }
+          if (currencyId.eq(nativeCurrencyId)) {
+            return api.query.system.account<AccountInfo>(address).pipe(
+              map((result) => {
+                return result.data.free;
+              })
+            );
+          }
+
+          const key1 = api.query.tokens.accounts.creator.meta.type.asDoubleMap.key1.toString();
+          const arg = key1 === 'CurrencyId' ? [token, address] : [address, token];
+          return api.query.tokens.accounts<OrmlAccountData>(...arg).pipe(
+            map((result) => {
+              return result.free;
+            })
+          );
+        })
+      );
     }
   );
 }
