85dbcbb0f8110bc86b5d2acf62683cbcd8d7ffa1
Nantian <nxiiun@gmail.com>
2021-03-29 16:06:24 +1300

    fix getType

---
+++ packages/api-mobx/src/getType.ts
@@ -0,0 +1,46 @@
+// Copyright 2017-2021 @polkadot/types authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { StorageEntryTypeLatest } from '@polkadot/types/interfaces/metadata';
+import { StorageKey } from '@polkadot/types/primitive/StorageKey';
+import type { StorageEntry } from '@polkadot/types/primitive/types';
+import type { InterfaceTypes } from '@polkadot/types/types';
+import { isFunction } from '@polkadot/util';
+
+function getStorageType(type: StorageEntryTypeLatest): [boolean, string] {
+  if (type.isPlain) {
+    return [false, type.asPlain.toString()];
+  } else if (type.isDoubleMap) {
+    return [false, type.asDoubleMap.value.toString()];
+  }
+
+  return [false, type.asMap.value.toString()];
+}
+
+// we unwrap the type here, turning into an output usable for createType
+/** @internal */
+export function unwrapStorageType(type: StorageEntryTypeLatest, isOptional?: boolean): keyof InterfaceTypes {
+  const [hasWrapper, outputType] = getStorageType(type);
+
+  return isOptional && !hasWrapper
+    ? (`Option<${outputType}>` as keyof InterfaceTypes)
+    : (outputType as keyof InterfaceTypes);
+}
+
+/** @internal */
+export function getType(value: StorageKey | StorageEntry | [StorageEntry, any]): string {
+  if (value instanceof StorageKey) {
+    return value.outputType;
+  } else if (isFunction(value)) {
+    return unwrapStorageType(value.meta.type);
+  } else if (Array.isArray(value)) {
+    const [fn] = value;
+
+    if (fn.meta) {
+      return unwrapStorageType(fn.meta.type);
+    }
+  }
+
+  // If we have no type set, default to Raw
+  return 'Raw';
+}
