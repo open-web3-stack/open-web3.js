c55f6f2a37ad25d0861f31c693ee520807d62a05
Stefan Popov <stefanpopov@list.ru>
2021-06-04 17:11:41 +0400

    Fix types alias objects concatenation for complex objects

--- packages/orml-type-definitions/src/utils/typesAliasFromDefs.ts
+++ packages/orml-type-definitions/src/utils/typesAliasFromDefs.ts
@@ -1,12 +1,11 @@
+import merge from 'lodash/merge'
+
 export default function typesAliasFromDefs(
   definitions: Record<string, Record<string, any>>,
   initAlias: Record<string, any> = {}
 ): Record<string, any> {
   return Object.values(definitions).reduce(
-    (res: Record<string, any>, { typesAlias }): Record<string, any> => ({
-      ...res,
-      ...typesAlias
-    }),
+    (res: Record<string, any>, { typesAlias }): Record<string, any> => merge(typesAlias, res),
     initAlias
   );
 }
