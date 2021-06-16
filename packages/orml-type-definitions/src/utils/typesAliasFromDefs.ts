6138c7431c30fbae41e8c098e3eaaa56da474561
Stefan Popov <stefanpopov@list.ru>
2021-06-16 14:52:19 +0400

    Fix original object mutation

--- packages/orml-type-definitions/src/utils/typesAliasFromDefs.ts
+++ packages/orml-type-definitions/src/utils/typesAliasFromDefs.ts
@@ -5,7 +5,7 @@ export default function typesAliasFromDefs(
   initAlias: Record<string, any> = {}
 ): Record<string, any> {
   return Object.values(definitions).reduce(
-    (res: Record<string, any>, { typesAlias }): Record<string, any> => merge(typesAlias, res),
+    (res: Record<string, any>, { typesAlias }): Record<string, any> => merge({}, typesAlias, res),
     initAlias
   );
 }
