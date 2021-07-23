d04f686a040fd117630d874fc76f0ae0a0304d59
Dastan <dastanbeksamatov@gmail.com>
2021-07-23 09:32:17 +0300

    Fix indexer fails with `system.set_code()` extrinsic  (#192)
    * Fix sudo param name
    
    * Fix address prefix
    
    * Remove types
    
    * Resolve comments
    
    * Resolve comments
    
    * fix linter issue
    
    * Remove api instantiation
    
    * Update scanner
    
    * Resolve comment

--- packages/scanner/src/Scanner.ts
+++ packages/scanner/src/Scanner.ts
@@ -194,6 +194,11 @@ class Scanner {
     }
   }
 
+  public async getParentHash(_blockHash?: Bytes): Promise<Bytes> {
+    const header = await this.rpcProvider.send('chain_getHeader', _blockHash ? [_blockHash] : []);
+    return header.parentHash as Bytes;
+  }
+
   public getSpecTypes(version: RuntimeVersion) {
     const types = getSpecTypes(
       {
@@ -211,7 +216,8 @@ class Scanner {
 
   public async getChainInfo(_blockAt?: BlockAtOptions): Promise<ChainInfo> {
     const { blockHash, blockNumber } = await this.getBlockAt(_blockAt);
-    const runtimeVersion = await this.getRuntimeVersion(blockHash);
+    const parentHash = await this.getParentHash(blockHash);
+    const runtimeVersion = await this.getRuntimeVersion(parentHash);
     const cacheKey = `${runtimeVersion.specName}/${runtimeVersion.specVersion}`;
     if (!this.chainInfo[cacheKey]) {
       const registry = new TypeRegistry();
@@ -223,7 +229,7 @@ class Scanner {
       // eslint-disable-next-line
       if (!this.metadataRequest[cacheKey]) {
         this.metadataRequest[cacheKey] = this.rpcProvider
-          .send('state_getMetadata', [blockHash])
+          .send('state_getMetadata', [parentHash])
           .then((rpcdata: string) => {
             const metadata = new Metadata(registry, rpcdata);
 
