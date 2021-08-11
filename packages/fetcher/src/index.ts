17df7c603a42a4759b3c511820e8f2d09ad167e8
Ermal Kaleci <ermalkaleci@gmail.com>
2021-08-11 14:27:21 +0200

    update exports

--- packages/fetcher/src/index.ts
+++ packages/fetcher/src/index.ts
@@ -1,6 +1,8 @@
 export { default as AlphaVantage } from './alpha-vantage';
 
-export { PriceFetcher } from './types';
+export { PriceFetcher, Trade, TradesFetcher } from './types';
 export { default as CCXTFetcher } from './ccxt-fetcher';
 export { default as CryptoCompareFetcher } from './crypto-compare-fetcher';
 export { default as CombinedFetcher } from './combined-fetcher';
+export { default as CoinGeckoFetcher } from './coingecko-fetcher';
+export { default as VWAPFetcher } from './vwap-fetcher';
